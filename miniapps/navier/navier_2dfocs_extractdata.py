9741f6f1b1dc250cb2bc0899680d132b2279a3b3
William Anderson <53445030+andersonw1@users.noreply.github.com>
2024-07-03 14:48:02 -0700

    Add files for data generation

--- miniapps/navier/navier_2dfocs_extractdata.py
+++ miniapps/navier/navier_2dfocs_extractdata.py
@@ -3,11 +3,12 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from pathlib import Path
+from mpi4py import MPI
 
 def extract_data(kinvis, resolution, numprocs, dirname):
-    # TODO
     # Run simulation with given Kinematic viscosity, resolution,
     # and numprocs and save data to npy files
+    # ==> This is done in navier_2dfocs_generatedata.py
 
     # Load data from npy files split across processes and 
     # concatenate into single npy file
@@ -21,41 +22,62 @@ def extract_data(kinvis, resolution, numprocs, dirname):
     
     for i in range(numprocs):
         smyid = '{:0>6d}'.format(i)
-        # nodevals = np.load(f'{dirname}/Res_{resolution}_kinvis_{kinvis}_nodessmall2{smyid}.npy', allow_pickle=True)
-        # velvals = np.load(f'{dirname}/Res_{resolution}_kinvis_{kinvis}_velssmall2{smyid}.npy', allow_pickle=True)
         nodevals = np.load(f'{dirname}/nodes_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
-        velvals = np.load(f'{dirname}/velocities_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
+        velvals = np.load(f'{dirname}/velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
         pressvals = np.load(f'{dirname}/pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
         
         #mfem outputs single vector for nodes, 
         #where first half is x-coords and second half is y-coords
-        halfpt = halfpt + [round(len(nodevals )/2)]
+        halfpt = halfpt + [round(len(nodevals)/2)]
         x = x + [nodevals[0:halfpt[i]]]
         y = y + [nodevals[halfpt[i]:]]
         velsx = velsx + [velvals[:,0:halfpt[i]]]
         velsy = velsy + [velvals[:,halfpt[i]:]]
+        pressure = pressure + [pressvals]
     
+    # Just to make the variables below clear:
+    # A node on the mesh is given by the coordinate pair (xall[i], yall[i]).
+    # If 2D velocity is given by the vector u = [u_1, u_2] 
+    # then at a given node, u_1 = velsxall[i] and u_2 = velsyall[i]
+
     xall = np.concatenate(x)    # x coordinates of velocity field
     yall = np.concatenate(y)    # y coordinates of velocity field
     velsxall = np.hstack(velsx) # x velocity values 
     velsyall = np.hstack(velsy) # y velocity values
+    pressureall = np.hstack(pressure) # y velocity values
 
+    # TODO: Dinos could update to hdf5 here
     np.save(f'{dirname}/xCoord_Res_{resolution}_kinvis_{kinvis}.npy', xall)
     np.save(f'{dirname}/yCoord_Res_{resolution}_kinvis_{kinvis}.npy', yall)
     np.save(f'{dirname}/xVelocity_Res_{resolution}_kinvis_{kinvis}.npy', velsxall)
     np.save(f'{dirname}/yVelocity_Res_{resolution}_kinvis_{kinvis}.npy', velsyall)
+    np.save(f'{dirname}/Pressure_Res_{resolution}_kinvis_{kinvis}.npy', pressureall)
+
     
 
 if __name__ == '__main__':
   my_parser = argparse.ArgumentParser()
-  my_parser.add_argument("--kinvis", type=float, default=1e2, help="Reynold's number for current simulation")
-  my_parser.add_argument("--resolution", type=str, default='high', help="Resolution of simulation (either `high` or `low`)")
-  my_parser.add_argument("--numprocs", type=int, default=8, help="Number of processes when running simulation")
-  my_parser.add_argument("--dirname", type=str, default='/usr/workspace/zeroml/mfem-data', help="Directory in which to save results")
+  my_parser.add_argument("--kinvis", 
+                          type=float, 
+                          default=1e2, 
+                          help="Reynold's number for current simulation")
+  my_parser.add_argument("-res", "--resolution", 
+                          type=str, 
+                          default='high', 
+                          help="Resolution of simulation (either `high` or `low`)")
+  my_parser.add_argument("--numprocs", 
+                          type=int, 
+                          default=8, 
+                          help="Number of processes when running simulation")
+  my_parser.add_argument("--dirname", 
+                          type=str, 
+                          default='/usr/workspace/zeroml/mfem-data', 
+                          help="Directory in which to save results")
   args = my_parser.parse_args()
 
   # Update directory name based on Kinematic viscosity/Resolution
-  my_dirname = f'{args.dirname}/Resolution-{args.resolution}/kinvis-{args.kinvis}'
+  my_dirname = f'{args.dirname}/resolution-{args.resolution}/kinvis-{args.kinvis}'
+  print(f"Creating directory {my_dirname} (if is does not already exist)")
   Path(my_dirname).mkdir(parents=True, exist_ok=True)
 
   # Run single simulation with given Kinematic viscosity and resolution
--- miniapps/navier/navier_2dfocs_generatedata.py
+++ miniapps/navier/navier_2dfocs_generatedata.py
@@ -16,14 +16,18 @@ import sys
 from os.path import expanduser, join
 from numpy import sin, cos, exp, sqrt, zeros, abs, pi
 import numpy as np
+from pathlib import Path
 from mpi4py import MPI
+import navier_2dfocs_downsample
+from scipy.interpolate import griddata
+import ctypes
 
 #set up simulation to run in parallel
 num_procs = MPI.COMM_WORLD.size
 myid = MPI.COMM_WORLD.rank
 smyid = '{:0>6d}'.format(myid)
    
-def run(ser_ref_levels=0,
+def run(ser_ref_levels=1,
         order=4,
         kinvis=0.001,
         t_final = 5,
@@ -32,15 +36,23 @@ def run(ser_ref_levels=0,
         ni = False,
         visualization = False,
         numba = True,
+        dirname = "./",
+        skipsteps = 0,
         resolution = 0):
     
+    if resolution == 0:
+        downsample = True
+    else:
+        downsample = False
+    
+    if downsample == True and ser_ref_levels == 0 and myid == 0:
+         print('Exiting: ser_ref_levels = 0 so we cannot downsample. Must set -rs 1 or greater if we want to downsample.')
+         sys.exit()
+
     vels = []
     
     #choose mesh
-    if resolution == 0: #low res
-         mesh = mfem.Mesh("rect-cylinder_lowres.msh")
-    else: #hi res
-         mesh = mfem.Mesh("rect-cylinder_hires.msh")
+    mesh = mfem.Mesh("rect-cylinder.msh")
 
     # refine mesh in MFEM.
     # currently ser_ref_levels default is 0 (no refinement)
@@ -55,7 +67,7 @@ def run(ser_ref_levels=0,
     #Setup navier solver with partial assembly
     flowsolver = mfem.navier_solver.NavierSolver(pmesh,order,kinvis)
     flowsolver.EnablePA(pa)
-
+    
     # I think the below lines are unclear in python. 
     # u_ic points to the current flowsolver velocity, which means
     # changing u_ic sets the velocity of the flowsolver.
@@ -89,34 +101,38 @@ def run(ser_ref_levels=0,
     attr[4] = 1 #cylinder
     flowsolver.AddVelDirichletBC(u_excoeff, attr)
 
+
     #Flowsolver setup
     time = 0.0
     last_step = False
 
     flowsolver.Setup(dt)
 
+
     u_gf = flowsolver.GetCurrentVelocity()
     p_gf = flowsolver.GetCurrentPressure()
-    
     
     #save velocity and pressure grid functions
     sol_name_v = f'nav2dv_{resolution}_kinvis_{kinvis}.'+smyid
     sol_name_p = f'nav2dp_{resolution}_kinvis_{kinvis}.'+smyid
     
     u_gf.Save(sol_name_v, 16)
-    p_gf.Save(sol_name_v, 16)
+    p_gf.Save(sol_name_p, 16)
 
-    #save numpy arrays
-    np.save(f'velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',u_gf.GetDataArray(), allow_pickle=True)
-    np.save(f'pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',p_gf.GetDataArray(), allow_pickle=True)
+    #save numpy arrays is skipsteps is zero, otherwise IC is saved
+    #at end of skipsteps
+    if skipsteps == 0:
+        np.save(f'{dirname}/velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',u_gf.GetDataArray(), allow_pickle=True)
+        np.save(f'{dirname}/pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',p_gf.GetDataArray(), allow_pickle=True)
 
     #get/save the nodes on each processor
     velgf = mfem.ParGridFunction(pmesh,sol_name_v)
     nodes = mfem.ParGridFunction(velgf)
     pmesh.GetNodes(nodes)
     coords =  nodes.GetDataArray()
-
-    np.save(f'nodes_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',coords)
+    np.save(f'{dirname}/nodes_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',coords, allow_pickle=True)
+    os.remove(sol_name_v)
+    os.remove(sol_name_p)
     
     step = 0
 
@@ -130,9 +146,10 @@ def run(ser_ref_levels=0,
          pvdc.RegisterField("velocity", u_gf)
          pvdc.RegisterField("pressure", p_gf)
          pvdc.Save()
+         
 
-    while last_step == False:
-        if time + dt >= t_final - dt/2:
+    while last_step == False and skipsteps > 0:
+        if time + dt >= skipsteps*dt - dt/2:
             last_step = True
 
         
@@ -143,14 +160,11 @@ def run(ser_ref_levels=0,
         #reloading the array at every timestep. But, I don't know how to 
         #dereference pointers in PyMFEM so this will work for now
 
-        vel = np.load(f'velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
-        vel = np.vstack((vel,u_gf.GetDataArray()))
-        np.save(f'velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',np.array(vel, dtype=object), allow_pickle=True)
-            
-        pressure = np.load(f'pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
-        pressure = np.vstack((pressure,p_gf.GetDataArray()))
-        np.save(f'pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',np.array(pressure, dtype=object), allow_pickle=True)
-
+        #save last timestep as IC
+        if last_step:
+            np.save(f'{dirname}/velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',np.array(u_gf.GetDataArray(), dtype=object), allow_pickle=True)
+            np.save(f'{dirname}/pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',np.array(p_gf.GetDataArray(), dtype=object), allow_pickle=True)
+        
         if visualization and step % 10 == 0:
             pvdc.SetCycle(step)
             pvdc.SetTime(time)
@@ -160,15 +174,105 @@ def run(ser_ref_levels=0,
              print(" "*7 + "Time" + " "*10 + "dt" )
              print(f'{time:.5e} {dt:.5e} \n')
 
-
         step = step + 1
+
+
     
-    flowsolver.PrintTimingData()
+    # flowsolver.PrintTimingData()
+
+    # if downsampling, we define a new flowsolver_ds  on the low-res mesh
+    # and reinitialize everything
+    if downsample == True:
+        mesh_ds = mfem.Mesh("rect-cylinder.msh")
+        for i in range(ser_ref_levels-1):
+            mesh_ds.UniformRefinement()
+        pmesh_ds = mfem.ParMesh(MPI.COMM_WORLD, mesh_ds) #parallel mesh
+        flowsolver_ds = mfem.navier_solver.NavierSolver(pmesh_ds,order,kinvis)
+        flowsolver_ds.EnablePA(pa)
+
+        u_ic_ds = flowsolver_ds.GetCurrentVelocity() 
+        p_ic_ds = flowsolver_ds.GetCurrentPressure()
+        
+        u_ic_ds.Save(f'nav2dv_{resolution}_kinvis_{kinvis}_ds_{smyid}', 16)
+
+        #get the nodes on each processor
+        velgf_ds = mfem.ParGridFunction(pmesh_ds,f'nav2dv_{resolution}_kinvis_{kinvis}_ds_{smyid}')
+        nodes_ds = mfem.ParGridFunction(velgf_ds)
+        pmesh_ds.GetNodes(nodes_ds)
+        coords_ds =  nodes_ds.GetDataArray()
+        np.save(f'{dirname}/nodes_resolution_{resolution}_kinvis_{kinvis}_ds_{smyid}.npy',coords_ds, allow_pickle=True)
+        os.remove(f'nav2dv_{resolution}_kinvis_{kinvis}_ds_{smyid}')
+
+        MPI.COMM_WORLD.Barrier() #make sure all coordinate files are saved for each processor
+        vel, pressure = navier_2dfocs_downsample.downsample_data(kinvis, resolution, num_procs, myid, dirname)
+        MPI.COMM_WORLD.Barrier() #make sure we have vel/pressure defined
+        
+        #remove old coord file, save new one
+        os.remove(f'{dirname}/nodes_resolution_{resolution}_kinvis_{kinvis}_ds_{smyid}.npy')
+        np.save(f'{dirname}/nodes_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',coords_ds, allow_pickle=True)
+
+        u_ic_ds.Assign(mfem.Vector(vel.astype(float)))
+        p_ic_ds.Assign(mfem.Vector(pressure.astype(float)))
+
+        u_gf.Destroy()
+        p_gf.Destroy()
+
+        u_gf = flowsolver_ds.GetCurrentVelocity()
+        p_gf = flowsolver_ds.GetCurrentPressure()   
+
+        np.save(f'{dirname}/velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',u_ic_ds.GetDataArray(), allow_pickle=True)
+        np.save(f'{dirname}/pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',p_ic_ds.GetDataArray(), allow_pickle=True)
+
+
+        attr_ds = intArray(pmesh_ds.bdr_attributes.Max())
+        attr_ds[0] = 1 #inlet
+        attr_ds[4] = 1 #cylinder
+        flowsolver_ds.AddVelDirichletBC(u_excoeff, attr_ds)
+        flowsolver_ds.Setup(dt)
+        time_ds = 0
+        step_ds = 0
+
     
-    # mesh_name = "nav2d-mesh."+smyid
-    # pmesh.Print(mesh_name, 16)
-    # u_gf.Save(sol_name_v, 16)
-    # p_gf.Save(sol_name_p, 16)
+    last_step = False
+    
+    vel = np.load(f'{dirname}/velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
+    pressure = np.load(f'{dirname}/pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy', allow_pickle=True)
+ 
+
+    while last_step == False:
+        if downsample == True:
+            if time_ds + dt >= t_final - dt/2:
+                last_step = True
+            time_ds = flowsolver_ds.Step(time_ds, dt, step_ds) #t should update in here
+        else:
+            if time + dt >= t_final + skipsteps*dt - dt/2:
+                last_step = True
+            time = flowsolver.Step(time, dt, step) #t should update in here
+
+        vel = np.vstack((vel,np.array((ctypes.c_double * u_gf.Size()).from_address(int(u_gf.GetData())), copy=True)))
+        pressure = np.vstack((pressure,np.array((ctypes.c_double * p_gf.Size()).from_address(int(p_gf.GetData())), copy=True)))
+
+
+        if visualization and step % 10 == 0:
+            pvdc.SetCycle(step)
+            pvdc.SetTime(time)
+            pvdc.Save()
+
+        if MPI.ROOT:
+             print(" "*7 + "Time" + " "*10 + "dt" )
+             if downsample:
+                 print(f'{time_ds + skipsteps*dt:.5e} {dt:.5e} \n')
+                 step_ds = step_ds + 1
+             else:
+                 print(f'{time:.5e} {dt:.5e} \n')
+                 step = step + 1
+
+    np.save(f'{dirname}/velocity_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',np.array(vel, dtype=object), allow_pickle=True)
+    np.save(f'{dirname}/pressure_resolution_{resolution}_kinvis_{kinvis}_{smyid}.npy',np.array(pressure, dtype=object), allow_pickle=True)
+
+
+    print('Finished Sim')
+
 
 
 if __name__ == "__main__":
@@ -177,7 +281,7 @@ if __name__ == "__main__":
     parser = ArgParser(description='navier_mms (translated from miniapps/navier/navier_mms.cpp)')
 
     parser.add_argument('-rs', '--refine-serial',
-                        action='store', default=0, type=int,
+                        action='store', default=1, type=int,
                         help="Number of times to refine the mesh uniformly in serial.")
     parser.add_argument('-o', '--order',
                         action='store', default=4, type=int,
@@ -203,12 +307,25 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--numba",
                         default=1, action='store', type=int,
                         help="Use Number compiled coefficient")
+    parser.add_argument("--dirname", 
+                        type=str,
+                        default='/usr/workspace/zeroml/mfem-data', 
+                        help="Directory in which to save results")
+    parser.add_argument("--skipsteps",
+                        default=0,action='store', type=int,
+                        help='Number of timesteps to skip before downsampling')
     parser.add_argument("-res", "--resolution",
                         default=0,action='store', type=int,
                         help='Mesh file to use. Zero for low res, otherwise hi-res')
+ 
    
     args = parser.parse_args()
     parser.print_options(args)
+
+    # Update directory name based on Kinematic viscosity/Resolution
+    my_dirname = f'{args.dirname}/resolution-{args.resolution}/kinvis-{args.kinvis}'
+    print(f"Creating directory {my_dirname} (if it does not already exist)")
+    Path(my_dirname).mkdir(parents=True, exist_ok=True)
 
     numba = (args.numba == 1)
 
@@ -221,5 +338,6 @@ if __name__ == "__main__":
         ni=False,
         visualization=args.visualization,
         numba=numba,
-        resolution=args.resolution)
-
+        dirname=my_dirname,
+        skipsteps = args.skipsteps,
+        resolution = args.resolution)
